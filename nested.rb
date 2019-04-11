40ce416be2cde14a30048b5ba074471dde1d7062
Caleb Einarr <earthctzn1@gmail.com>
2019-04-11 01:24:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -72,12 +72,10 @@ def adding_matz
           :languages => ["C"]
         }
      },
-      programmer_hash[:yukihiro_matsumoto => {
-        :known_for => "Ruby",
-        :languages => ["LISP", "C"]
-        }
-    ]
-      programmer_hash
+programmer_hash[:yukihiro_matsumoto] = { 
+  :known_for => "Ruby", 
+  :languages => ["LISP", "C"] }
+programmer_hash
   end
 
 def changing_alan
