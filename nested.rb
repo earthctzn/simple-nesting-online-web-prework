544250c7ee12b738d1efd9e22c6876b01be4a5a7
Caleb Einarr <earthctzn1@gmail.com>
2019-04-11 01:14:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,7 @@ def dennis_ritchies_language
   end
 
 def adding_matz
-
-	programmer_hash = 
+  programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -73,7 +72,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-   programmer_hash[:yukihiro_matsumoto => {
+    programmer_hash[:yukihiro_matsumoto => {
       :known_for => "Ruby",
       :languages => ["LISP", "C"]
     }
