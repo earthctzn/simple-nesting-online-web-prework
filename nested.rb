f1ed77f2aab0199e44369253bf40a3b3131be25e
Caleb Einarr <earthctzn1@gmail.com>
2019-04-11 00:42:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -118,5 +118,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages][0]
+    new_hash = programmer_hash[:dennis_ritchie][:languages]
+    new_hash[:languages][0]
   end
